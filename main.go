0848ff8aa9363d06f28630c688c613cf94e794f8
佐橋周作 <13200387+Shusaku-Sahashi@users.noreply.github.com>
2019-06-30 10:58:57 +0900

    mainからDBが呼び出されるように変更

--- main.go
+++ main.go
@@ -4,10 +4,29 @@ import (
 	"log"
 	"os"
 
-	"github.com/app/realworld/router"
+	"github.com/app/realworld/db"
+
+	"github.com/app/realworld/config"
 )
 
 func init() {
+	// loggerの初期化
+	setLogger()
+}
+
+func main() {
+	// 各種設定情報の初期化
+	config.InitializeConf()
+	db := db.DBConn()
+	defer db.Close()
+
+	//r := handler.InitRouter()
+	//if err := r.Run(); err != nil {
+	//log.Panic(err)
+	//}
+}
+
+func setLogger() {
 	// TODO: logパッケージとして切り出す。
 	errorLog, err := os.OpenFile("Error.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
@@ -18,11 +37,4 @@ func init() {
 	// 時刻と時刻のマイクロ秒、ディレクトリパスを含めたファイル名を出力
 	log.SetFlags(log.Ltime | log.Lmicroseconds | log.Llongfile)
 	log.SetPrefix("[error]:")
-}
-
-func main() {
-	r := router.InitRouter()
-	if err := r.Run(); err != nil {
-		log.Panic(err)
-	}
 }
